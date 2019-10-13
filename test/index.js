ea36b39823b6cd11ead3823d1584b20c21b7e61b
Michael Hobbs <michael.lee.hobbs@gmail.com>
2019-10-13 09:48:11 -0400

    updated test

--- test/index.js
+++ test/index.js
@@ -7,15 +7,18 @@ const dateFormat = 'YYYY.MM.DD'
 
 let command = `node src/index.js --output ./test/test --files ./test/testfiles.pdfm`
 let outfile = path.resolve(`./test/test-${moment().format(dateFormat)}.pdf`)
+let expected = path.resolve(`./test/expected.pdf`)
 exec(command, async (error, stdout, stderr) => {
         if (error) {
             console.error(error)
         } else {
             let file = fs.readFileSync(outfile)
+            let expectedFile = fs.readFileSync(expected)
             // todo
             // md5()
             console.log(stdout)
             console.log(md5(file))
+            console.log(expectedFile.equals(file))
         }
     }
 )
